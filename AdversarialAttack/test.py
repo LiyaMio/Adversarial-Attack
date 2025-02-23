15b349422a815d8863191375f0314cfb8dc924ab
LiyaMio <2576622848@qq.com>
2025-02-24 07:58:29 +0800

    fgsm

--- AdversarialAttack/test.py
+++ AdversarialAttack/test.py
@@ -24,13 +24,6 @@ class SimpleCNN(nn.Module):
         return x
 
 
-def fgsm_attack(image, epsilon, data_grad):
-    sign_data_grad = data_grad.sign()
-    perturbed_image = image + epsilon * sign_data_grad
-    perturbed_image = torch.clamp(perturbed_image, 0, 1)
-    return perturbed_image
-
-
 def imshow(img, title, ax):
     img = img.squeeze().numpy()
     ax.imshow(img, cmap='gray')
